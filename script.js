906c68db1c875d510b31db295511a1a7f9f76828
DrZebr <160625370+DrZebr@users.noreply.github.com>
2024-05-03 21:50:53 -0400

    broken button

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Hello, World!')
+// Function to toggle the menu content visibility
+function toggleMenu() {
+    var menuContent = document.getElementById("menuContent");
+    menuContent.classList.toggle("show"); // Toggle the "show" class
+}
+
+// Function to hide the menu content when clicking outside of it
+window.onclick = function(event) {
+    var menuIcon = document.querySelector('.menu-icon');
+    var menuContent = document.getElementById("menuContent");
+    if (!event.target.matches('.menu-icon') && !event.target.matches('.menu-content') && !menuContent.contains(event.target)) {
+        menuContent.classList.remove('show'); // Remove the "show" class
+    }
+}
